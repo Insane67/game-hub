e5b15f7805ad0c772a487019853f643a6428fd00
insane67 <s.arslan6767@outlook.com>
2023-07-20 00:46:17 +0300

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+//özel configurasyon ile axios un instance ını yaratırız.
+export default axios.create({
+  baseURL:"https://api.rawg.io/api",
+  params: {
+    key: "bf23279a442341fa89c97ad1be3b3b29",
+  },
+});
