cb1f085d93f661f6567b427c17ea1fa2397361ce
insane67 <s.arslan6767@outlook.com>
2023-07-25 02:25:40 +0300

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,7 +16,19 @@ export interface Game {
   genrics: Genre[];
 }
 
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genres: selectedGenre?.id } },[selectedGenre?.id]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        parent_platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 export default useGames;
